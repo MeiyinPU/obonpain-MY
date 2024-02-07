9ebf63a0d367d98bd489b978656533e2e8de7ff1
MeiyinPU <mei-yin.pu@oclock.school>
2024-02-07 16:26:59 +0100

    add Header Footer PageAccueil

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.scss'
+import  PageAccueil  from './composants/PageAccueil/PageAccueil.jsx'
+
+
+function App() {
+ 
+
+  return (
+    <div>
+      <PageAccueil />
+    </div>
+    
+  )
+}
+
+export default App
